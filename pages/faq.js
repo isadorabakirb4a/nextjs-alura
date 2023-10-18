f31ce4d55654cd1962e5f0eff8eb65876932770e
Isadora Bakir <isadora.bakir@b4a.com.br>
2023-10-18 09:46:13 -0300

    feat: init develop

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link"
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Isadora's site - FAQ page</h1>
+            <Link href="/">
+                Ir para Home
+            </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link"
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Isadora's site - Home page</h1>
+            <Link href="/faq">
+                Ir para FAQ
+            </Link>
+        </div>
+    )
+}
