5ae5c02eaba65dc251e36e6a063d55e2fbf3fcb2
Isadora Bakir <isadora.bakir@b4a.com.br>
2023-10-20 14:22:59 -0300

    feat: add head and font

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,12 @@
 import Link from "../src/components/Link"
+import Head from 'next/head';
 
 export default function FAQPage() {
     return (
         <div>
+            <Head>
+                <title>FAQ - Alura Cases Campanha</title>
+            </Head>
             <h1>Isadora's site - FAQ page</h1>
             <Link href="/">
                 Ir para Home
