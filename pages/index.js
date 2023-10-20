daa6754c54e4a15301d3f1d34e7ed13f98601496
Isadora Bakir <isadora.bakir@b4a.com.br>
2023-10-20 10:52:17 -0300

    feat: add components, global style, and other

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,3 @@
-import Link from "../src/components/Link"
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-    return (
-        <div>
-            <h1>Isadora's site - Home page</h1>
-            <Link href="/faq">
-                Ir para FAQ
-            </Link>
-        </div>
-    )
-}
+export default HomeScreen;
